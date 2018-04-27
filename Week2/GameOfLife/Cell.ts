89e1fd310d8b6d2ed6d9a0f5c887998edefe2306
minhphuong99 <38272383+minhphuong99@users.noreply.github.com>
2018-04-27 11:58:48 +0700

    Game of life ver 4

--- Week2/GameOfLife/Cell.ts
+++ Week2/GameOfLife/Cell.ts
@@ -3,14 +3,20 @@ export class Cell{
     public row;
     public column;
     public state;
+    public friend;
 
-    public constructor(row, column, state = CELL_STATE.DEAD) {
+    public constructor(row, column, state = CELL_STATE.DEAD, friend = 0) {
         this.row = row;
         this.column = column;
         this.state = state;
+        this.friend = friend;
     }
 
     public setState(state) {
         this.state = state;
+    }
+
+    public setFriend(friend){
+        this.friend = friend;
     }
 }
--- Week2/GameOfLife/GameBoard.ts
+++ Week2/GameOfLife/GameBoard.ts
@@ -3,20 +3,24 @@ import {Randomize} from "../../libs/Randomize";
 import {CELL_STATE, THREE_NEIGHBOURS, TWO_NEIGHBOURS} from "./constants";
 export class GameBoard {
     private Cells;
+    private ROW;
+    private COL;
 
-    public constructor() {
-        //mặc định 10x10
+    public constructor(ROW, COL) {
+        
         this.Cells = {};
+        this.ROW = ROW;
+        this.COL = COL;
 
-        for (let row = 0; row < 10; row++) {
-            for (let column = 0; column < 10; column++) {
+        for (let row = 0; row < ROW; row++) {
+            for (let column = 0; COL < 10; column++) {
                 this.Cells[`${row}_${column}`] = new Cell(row, column, Randomize.randomInt(0, 1));
             }
         }
     }
 
     public getNeighborhoodOfCell(cell:Cell) {
-        return 0;
+        return cell.friend;
     }
 
     public calculateState(cell:Cell) {
@@ -28,6 +32,27 @@ export class GameBoard {
             return CELL_STATE.ALIVE;
         } else {
             return CELL_STATE.DEAD;
+        }
+    }
+
+    public countFriendCell(cell:Cell){
+        let friend = 0;
+
+        for(let x = cell.row - 1; x <= cell.row + 1; x++){
+            for(let y = cell.column - 1; y <= cell.column + 1; y++){
+
+                if(x >= 0 && y >= 0 && x!= cell.row && y != cell.column && x < this.ROW && y < this.COL){
+                    if(cell[`${x}_${y}`].state == CELL_STATE.ALIVE) friend++;
+                
+                }
+            }
+        }
+        cell.setFriend(friend);
+    }
+
+    public countFriend(){
+        for(let index in this.Cells){
+            this.Cells[index].friend = this.countFriendCell(this.Cells[index]);
         }
     }
 
