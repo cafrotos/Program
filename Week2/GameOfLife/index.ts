8045e53201814db02e97849a9eb0bd2e8fbfc97f
minhphuong99 <38272383+minhphuong99@users.noreply.github.com>
2018-04-28 11:02:31 +0700

    file index

---
+++ Week2/GameOfLife/index.ts
@@ -0,0 +1,15 @@
+import {GameBoard} from "./GameBoard";
+import {Cell} from "./Cell";
+import {Randomize} from "../../libs/Randomize";
+import {CELL_STATE} from "./constants";
+
+
+var GamePlay = new GameBoard(10, 10);
+
+
+GamePlay.getStateCell(4, 4, CELL_STATE.ALIVE)
+GamePlay.getStateCell(4, 5, CELL_STATE.ALIVE)
+GamePlay.getStateCell(4, 6, CELL_STATE.ALIVE)
+GamePlay.getStateCell(4, 7, CELL_STATE.ALIVE)
+GamePlay.getStateCell(4, 8, CELL_STATE.ALIVE)
+GamePlay.run();
